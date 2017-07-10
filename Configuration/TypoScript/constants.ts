383e8f07ecd2d15a5d642cf4d82c86270fb85403
faerietree <radagast@ciry.at>
2017-07-10 23:26:58 +0200

    TypoScript: Add constants.ts from backend and include.

---
+++ Configuration/TypoScript/constants.ts
@@ -0,0 +1,19 @@
+page.theme.copyright.enable = 0
+page.theme.breadcrumb = 0
+
+page.logo.file = fileadmin/aimeos_dist/logo.png
+page.logo.height = 30px
+page.logo.width >
+
+page.fluidtemplate.partialRootPath = EXT:aimeos/Resources/Private/Partials/Page/
+
+plugin.bootstrap_package.settings.less.brand-primary = #B0A060
+plugin.bootstrap_package.settings.less.navbar-height = 20px
+plugin.bootstrap_package.settings.less.navbar-default-color = #B0A060
+plugin.bootstrap_package.settings.less.navbar-default-link-color = lighten(@navbar-default-link-active-color,15%)
+plugin.bootstrap_package.settings.less.navbar-default-link-active-color = #B0A060
+plugin.bootstrap_package.settings.less.btn-default-color = #B0A060
+plugin.bootstrap_package.settings.less.btn-font-weight = bold
+plugin.bootstrap_package.settings.less.grid-gutter-width = 20px
+plugin.bootstrap_package.settings.less.container-tablet = (730px + @grid-gutter-width)
+plugin.bootstrap_package.settings.less.container-tablet = (740px + @grid-gutter-width)
